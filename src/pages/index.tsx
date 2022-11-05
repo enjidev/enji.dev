1792707de5029ac41d4e5f9458bb9c3d45b0e7b0
enjidev <me@enji.dev>
2022-11-05 09:04:55 +0700

    feat: add image to header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import {
 } from '@/components/Icons';
 
 import type { NextPage } from 'next';
+import Image from 'next/image';
 
 const Index: NextPage = () => {
   return (
@@ -28,62 +29,77 @@ const Index: NextPage = () => {
           }}
         ></div>
         <div className="content-wrapper">
-          {/* title */}
-          <h1 className="text-gray-600">
-            <span className="mb-3 block text-5xl font-bold md:mb-6 md:text-7xl">
-              hai! saya{' '}
-              <strong className="font-extrabold text-gray-900">énji</strong>,
-            </span>
-            <span className="block text-xl md:text-2xl">
-              seorang <strong>designer</strong> dan <strong>developer</strong>.
-            </span>
-          </h1>
-          {/* cta */}
-          <div className="mt-8 flex gap-2">
-            <Link href="/" className="button min-w-[128px]">
-              HIRE ME
-            </Link>
-            <Link href="/" className="button-outlined">
-              RESUME
-            </Link>
-          </div>
-          {/* tech stack */}
-          <div className="mt-32">
-            <p className="mb-2 text-sm text-gray-400">
-              tech stack/tools favorit saya saat ini:
-            </p>
-            <ul className="flex gap-4 text-gray-400">
-              <li>
-                <div className="hover:text-[#3178C6]">
-                  <TypeScriptIcon className="h-6 w-6" />
-                </div>
-              </li>
-              <li>
-                <div className="hover:text-[#61DAFB]">
-                  <ReactIcon className="h-6 w-6" />
-                </div>
-              </li>
-              <li>
-                <div className="hover:text-[#06B6D4]">
-                  <TailwindCssIcon className="h-6 w-6" />
-                </div>
-              </li>
-              <li>
-                <div className="hover:text-[#000000]">
-                  <NextJsIcon className="h-6 w-6" />
-                </div>
-              </li>
-              <li>
-                <div className="hover:text-[#F24E1E]">
-                  <FigmaIcon className="h-6 w-6" />
-                </div>
-              </li>
-              <li>
-                <div className="hover:text-[#000000]">
-                  <InkscapeIcon className="h-6 w-6" />
-                </div>
-              </li>
-            </ul>
+          <div className="relative">
+            {/* title */}
+            <h1 className="text-gray-600">
+              <span className="mb-3 block text-5xl font-bold md:mb-6 md:text-7xl">
+                hai! saya{' '}
+                <strong className="font-extrabold text-gray-900">énji</strong>,
+              </span>
+              <span className="block text-xl md:text-2xl">
+                seorang <strong>designer</strong> dan <strong>developer</strong>
+                .
+              </span>
+            </h1>
+            {/* cta */}
+            <div className="mt-8 flex gap-2">
+              <Link href="/" className="button min-w-[128px]">
+                HIRE ME
+              </Link>
+              <Link href="/" className="button-outlined">
+                RESUME
+              </Link>
+            </div>
+            {/* tech stack */}
+            <div className="mt-32">
+              <p className="mb-2 text-sm text-gray-400">
+                tech stack/tools favorit saya saat ini:
+              </p>
+              <ul className="flex gap-4 text-gray-400">
+                <li>
+                  <div className="hover:text-[#3178C6]">
+                    <TypeScriptIcon className="h-6 w-6" />
+                  </div>
+                </li>
+                <li>
+                  <div className="hover:text-[#61DAFB]">
+                    <ReactIcon className="h-6 w-6" />
+                  </div>
+                </li>
+                <li>
+                  <div className="hover:text-[#06B6D4]">
+                    <TailwindCssIcon className="h-6 w-6" />
+                  </div>
+                </li>
+                <li>
+                  <div className="hover:text-[#000000]">
+                    <NextJsIcon className="h-6 w-6" />
+                  </div>
+                </li>
+                <li>
+                  <div className="hover:text-[#F24E1E]">
+                    <FigmaIcon className="h-6 w-6" />
+                  </div>
+                </li>
+                <li>
+                  <div className="hover:text-[#000000]">
+                    <InkscapeIcon className="h-6 w-6" />
+                  </div>
+                </li>
+              </ul>
+            </div>
+            {/* image */}
+            <div className="absolute right-0 -top-16 hidden lg:block">
+              {/* image */}
+              <Image
+                alt="me"
+                src="/me.png"
+                height={597}
+                width={467}
+                className="w-96"
+                priority
+              />
+            </div>
           </div>
         </div>
       </header>
