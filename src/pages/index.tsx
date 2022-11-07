d074174839a18ac5a8fe519fc248673e6edea3dc
enjidev <me@enji.dev>
2022-11-08 06:33:21 +0700

    chore: slightly reduce contrast of featured title text colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,9 @@ const FeaturedCard = ({ icon, title, desc }: FeaturedCardProps) => {
       <div className="absolute inset-y-0 inset-x-10 z-[-1] border-l border-slate-100 dark:border-slate-800"></div>
       <div className="mt-5 mr-4 ml-6 flex items-center gap-6 rounded-full bg-slate-100 dark:bg-slate-800">
         <div className="-m-2">{icon}</div>
-        <div className="truncate py-2 pr-4 font-bold">{title}</div>
+        <div className="truncate py-2 pr-4 font-bold text-gray-700 dark:text-slate-300">
+          {title}
+        </div>
       </div>
       <div className="pb-6 pl-16 pt-4 pr-6 text-sm text-gray-500 dark:text-slate-400">
         {desc}
