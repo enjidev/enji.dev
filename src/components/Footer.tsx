9918cfce053f43b69a3a7b02242f4333f151300f
enjidev <me@enji.dev>
2022-11-08 10:19:33 +0700

    feat: improve footer and add LocaleLink component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,39 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import LastCommit from '@/components/LastCommit';
 import dayjs from '@/utils/dayjs';
 
+type LocaleLinkProps = {
+  id: string;
+  title: string;
+  shortTitle: string;
+};
+
+const LocaleLink = ({ id, title, shortTitle }: LocaleLinkProps) => {
+  const { locale } = useRouter();
+  const isActive = locale === id;
+
+  return !isActive ? (
+    <Link href="/" locale={id} className="group">
+      <span className="group-hover:underline md:hidden">{shortTitle}</span>
+      <span className="hidden group-hover:underline md:inline-block">
+        {title}
+      </span>
+    </Link>
+  ) : (
+    <span className="font-semibold text-gray-800 dark:text-slate-200">
+      <span className="md:hidden">{shortTitle}</span>
+      <span className="hidden md:inline-block">{title}</span>
+    </span>
+  );
+};
+
 const Footer = () => {
   return (
-    <footer className="border-t border-slate-100 py-8 text-sm text-gray-900 dark:border-slate-800 dark:text-slate-200">
+    <footer className="border-t border-slate-100 text-sm text-gray-900 dark:border-slate-800 dark:text-slate-200">
       <div className="content-wrapper">
-        <section className="mb-12 font-semibold">
-          <ul className="flex flex-wrap justify-center gap-x-4 gap-y-1">
-            <li>
-              <Link href="/">Bahasa Indonesia</Link>
-            </li>
-            <li className="w-full sm:hidden"></li>
+        <section className="py-10 font-semibold">
+          <ul className="flex flex-wrap justify-center gap-x-4 gap-y-1 lg:gap-x-6">
             <li>
               <Link href="/">Design Concept</Link>
             </li>
@@ -23,14 +45,23 @@ const Footer = () => {
             </li>
           </ul>
         </section>
-        <section className="mb-2">
-          <div className="flex justify-center text-xs text-gray-500 hover:underline dark:text-slate-400">
-            <LastCommit />
-          </div>
-        </section>
-        <small className="flex justify-center text-sm font-semibold">
-          &copy; {dayjs().format('YYYY')}, Enji Kusnadi
-        </small>
+        <div className="flex justify-between border-t border-slate-100 py-6 text-sm dark:border-slate-800">
+          <section className="flex flex-row items-baseline gap-2">
+            <div className="font-semibold">
+              &copy; {dayjs().format('YYYY')}, Enji Kusnadi
+            </div>
+            <section className="group hidden text-gray-500 dark:text-slate-400 md:block">
+              <LastCommit withDot={true} />
+            </section>
+          </section>
+          <section className="flex flex-row items-baseline gap-2">
+            <div className="flex gap-2 text-gray-500 dark:text-slate-400">
+              <LocaleLink id="id" shortTitle="ID" title="Bahasa Indonesia" />
+              <div>&middot;</div>
+              <LocaleLink id="en" shortTitle="EN" title="English" />
+            </div>
+          </section>
+        </div>
       </div>
     </footer>
   );
--- src/components/LastCommit.tsx
+++ src/components/LastCommit.tsx
@@ -1,13 +1,25 @@
 import useLastCommit from '@/hooks/useLastCommit';
 import dayjs from '@/utils/dayjs';
 
-const LastCommit = () => {
+interface LastCommitProps {
+  withDot?: boolean;
+}
+
+const LastCommit = ({ withDot = false }: LastCommitProps) => {
   const { commit, isError } = useLastCommit();
 
   return commit && !isError ? (
-    <a href={commit.url} target="_blank" rel="noreferrer">
-      this site has been updated {dayjs(commit.date).fromNow()}.
-    </a>
+    <div>
+      {withDot && <span className="mr-2">&middot;</span>}
+      <a
+        href={commit.url}
+        target="_blank"
+        rel="noreferrer"
+        className="group-hover:underline"
+      >
+        this site has been updated {dayjs(commit.date).fromNow()}.
+      </a>
+    </div>
   ) : (
     <div>&nbsp;</div>
   );
