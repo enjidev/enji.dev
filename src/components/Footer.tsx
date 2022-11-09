d3a4c45766a8a0ebc40807e922f5600f7af938d8
enjidev <me@enji.dev>
2022-11-10 06:47:37 +0700

    refactor: add background color to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import dayjs from '@/utils/dayjs';
 
 const Footer = () => {
   return (
-    <footer className="border-t border-slate-100 text-sm text-gray-900 dark:border-slate-800 dark:text-slate-200">
+    <footer className="border-t border-slate-100 bg-white text-sm text-gray-900 dark:border-slate-800 dark:bg-slate-900 dark:text-slate-200">
       <div className="content-wrapper">
         <section className="py-10 font-semibold">
           <ul className="flex flex-wrap justify-center gap-x-4 gap-y-1 lg:gap-x-6">
