3b6803ece67f302a301bb4708fb7365874d7bb38
enjidev <me@enji.dev>
2022-11-09 18:22:26 +0700

    feat: declare website language to id

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
-    <Html>
+    <Html lang="id">
       <Head />
       <body>
         <Main />
