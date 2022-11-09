1c3211e73c6d12871cc035eee73d2f7a6a83cb40
enjidev <me@enji.dev>
2022-11-09 18:51:12 +0700

    feat: change website language to en

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
-    <Html lang="id">
+    <Html lang="en">
       <Head />
       <body>
         <Main />
