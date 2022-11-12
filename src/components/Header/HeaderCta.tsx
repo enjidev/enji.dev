39e1d531c18dc7ec26cc5a274efc937d331882df
enjidev <me@enji.dev>
2022-11-12 10:34:01 +0700

    feat: add isFree prop to header CTA

--- src/components/Header/HeaderCta.tsx
+++ src/components/Header/HeaderCta.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion, useReducedMotion } from 'framer-motion';
 import Link from 'next/link';
 import { DocumentIcon } from '@/components/Icons';
 
@@ -13,23 +13,100 @@ const animation = {
   },
 };
 
-const HeaderCta = () => {
+interface HeaderCtaProps {
+  isFree?: boolean;
+  isFreeAnimationDuration?: number;
+}
+
+const ButtonContactMe = () => {
+  return (
+    <Link href="/" className="button button--solid button--big min-w-[128px]">
+      Contact Me
+    </Link>
+  );
+};
+
+const ButtonResume = () => {
+  return (
+    <Link href="/" className="button button--ghost button--big px-2">
+      <DocumentIcon className="h-5 w-5" />
+      RESUME
+    </Link>
+  );
+};
+
+const HeaderCta = ({
+  isFree = true,
+  isFreeAnimationDuration = 5,
+}: HeaderCtaProps) => {
+  const shouldReduceMotion = useReducedMotion();
+
+  let isFreeVariants = {
+    hide: {
+      x: 0,
+      opacity: 1,
+    },
+    show: {
+      x: -48,
+      opacity: 0,
+    },
+  };
+
+  if (shouldReduceMotion) {
+    isFreeVariants = {
+      hide: {
+        x: 0,
+        opacity: 1,
+      },
+      show: {
+        x: 0,
+        opacity: 0,
+      },
+    };
+  }
+
   return (
     <motion.div className="flex gap-2" initial="hide" animate="show">
-      <motion.div variants={animation} transition={{ delay: 0.4 }}>
-        <Link
-          href="/"
-          className="button button--solid button--big min-w-[128px]"
+      <motion.div
+        className="relative z-20"
+        variants={animation}
+        transition={{ delay: 0.4 }}
+      >
+        <ButtonContactMe />
+      </motion.div>
+      {isFree ? (
+        <motion.div
+          variants={animation}
+          transition={{ delay: 0.5 }}
+          className="relative z-10"
         >
-          Contact Me
-        </Link>
-      </motion.div>
-      <motion.div variants={animation} transition={{ delay: 0.5 }}>
-        <Link href="/" className="button button--ghost button--big px-2">
-          <DocumentIcon className="h-5 w-5" />
-          RESUME
-        </Link>
-      </motion.div>
+          <motion.div
+            className="relative"
+            variants={isFreeVariants}
+            transition={{ delay: isFreeAnimationDuration, duration: 0.4 }}
+          >
+            <span className="absolute left-2.5 top-2.5 flex h-2.5 w-2.5">
+              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary-400 opacity-75 dark:bg-primary-300"></span>
+              <span className="relative inline-flex h-2.5 w-2.5 rounded-full bg-primary-500 dark:bg-primary-200"></span>
+            </span>
+            <div className="button button--ghost button--big pointer-events-none text-primary-400 dark:text-primary-200">
+              AVAILABLE FOR HIRE
+            </div>
+          </motion.div>
+          <motion.div
+            className="absolute top-0 left-0"
+            initial={{ x: -48, opacity: 0, pointerEvents: 'none' }}
+            animate={{ x: 0, opacity: 1, pointerEvents: 'auto' }}
+            transition={{ delay: isFreeAnimationDuration, duration: 0.4 }}
+          >
+            <ButtonResume />
+          </motion.div>
+        </motion.div>
+      ) : (
+        <motion.div variants={animation} transition={{ delay: 0.5 }}>
+          <ButtonResume />
+        </motion.div>
+      )}
     </motion.div>
   );
 };
