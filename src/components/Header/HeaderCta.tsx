ca7cc285834e364f2bc9008035e7128b84b17210
enjidev <me@enji.dev>
2022-11-12 13:42:23 +0700

    feat: improved header cta buttons

--- src/components/Header/HeaderCta.tsx
+++ src/components/Header/HeaderCta.tsx
@@ -20,7 +20,10 @@ interface HeaderCtaProps {
 
 const ButtonContactMe = () => {
   return (
-    <Link href="/" className="button button--solid button--big min-w-[128px]">
+    <Link
+      href="/"
+      className="button button--solid md:button--big min-w-[128px]"
+    >
       Contact Me
     </Link>
   );
@@ -30,11 +33,23 @@ const ButtonResume = () => {
   return (
     <Link
       href="/"
-      className="button button--ghost button--big button--underline px-2 after:left-2.5"
+      className="button button--ghost md:button--big button--underline px-2 after:left-2.5 md:px-2"
     >
       <DocumentIcon className="h-5 w-5" />
       RESUME
     </Link>
+  );
+};
+
+const AvailableForHire = () => {
+  return (
+    <div className="button button--ghost md:button--big pointer-events-none gap-2.5 px-2.5 text-primary-400 dark:text-primary-200 md:px-2.5">
+      <span className="relative flex h-2 w-2">
+        <span className="absolute -top-1 -left-1 inline-flex h-4 w-4 animate-ping rounded-full bg-primary-400 opacity-75 dark:bg-primary-300"></span>
+        <span className="relative inline-flex h-2 w-2 rounded-full bg-primary-500 dark:bg-primary-200"></span>
+      </span>
+      AVAILABLE FOR HIRE
+    </div>
   );
 };
 
@@ -84,17 +99,10 @@ const HeaderCta = ({
           className="relative z-10"
         >
           <motion.div
-            className="relative"
             variants={isFreeVariants}
             transition={{ delay: isFreeAnimationDuration, duration: 0.4 }}
           >
-            <span className="absolute left-3 top-3 flex h-2 w-2">
-              <span className="absolute -top-1 -left-1 inline-flex h-4 w-4 animate-ping rounded-full bg-primary-400 opacity-75 dark:bg-primary-300"></span>
-              <span className="relative inline-flex h-2 w-2 rounded-full bg-primary-500 dark:bg-primary-200"></span>
-            </span>
-            <div className="button button--ghost button--big pointer-events-none text-primary-400 dark:text-primary-200">
-              AVAILABLE FOR HIRE
-            </div>
+            <AvailableForHire />
           </motion.div>
           <motion.div
             className="absolute top-0 left-0"
