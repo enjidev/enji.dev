6b956c677e7489db2ba77503d889fda809abadbc
enjidev <me@enji.dev>
2022-11-12 12:59:49 +0700

    fix: header border bottom not quite visible on light mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import HeaderTechStack from '@/components/Header/HeaderTechStack';
 
 const Header = () => {
   return (
-    <header className="relative z-0 border-b border-b-slate-100 bg-slate-100 pt-20 pb-20 dark:border-divider-dark dark:bg-[#05011E] lg:pt-36 lg:pb-28">
+    <header className="relative z-0 border-b border-divider-light bg-slate-100 pt-20 pb-20 dark:border-divider-dark dark:bg-[#05011E] lg:pt-36 lg:pb-28">
       <div
         className="absolute inset-0 z-[-1] bg-slate-50 bg-grid-slate-200/60 dark:bg-slate-900 dark:bg-grid-slate-50/[0.04] lg:bg-grid-big-slate-200/50 lg:dark:bg-grid-big-slate-50/[.02]"
         style={{
