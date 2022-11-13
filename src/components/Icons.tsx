8c85570800ab22978dd409af158789345cdb6976
enjidev <me@enji.dev>
2022-11-13 07:32:41 +0700

    refactor: change logo to native html element

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -107,19 +107,6 @@ export const QuoteIcon = (props: ComponentProps<'svg'>) => {
   );
 };
 
-export const EnjiIcon = (props: ComponentProps<'svg'>) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      {...props}
-    >
-      <path d="m8 0c-4.4064 0-8 3.5936-8 8v8c0 4.4064 3.5936 8 8 8h8c4.4064 0 8-3.5936 8-8v-8c0-4.4064-3.5936-8-8-8h-8zm0 2h8c3.3256 0 6 2.6745 6 6v8c0 3.3256-2.6744 6-6 6h-8c-3.3255 0-6-2.6744-6-6v-8c0-3.3255 2.6745-6 6-6zm4.1914 3-3.1914 13.836h2.5566l3.1797-13.836h-2.5449z" />
-    </svg>
-  );
-};
-
 export const GitHubIcon = (props: ComponentProps<'svg'>) => {
   return (
     <svg
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,4 @@
 import clsx from 'clsx';
-import { EnjiIcon } from '@/components/Icons';
 
 interface LogoProps {
   active?: boolean;
@@ -10,13 +9,22 @@ const Logo = ({ active = false }: LogoProps) => {
     <div
       className={clsx('flex items-center gap-1.5 font-extrabold leading-none')}
     >
-      <EnjiIcon
-        className={clsx('h-6 w-6', [
-          active
-            ? 'text-primary-400 dark:text-primary-200'
-            : 'text-gray-900 dark:text-slate-200',
-        ])}
-      />
+      <div
+        className={clsx(
+          'flex h-6 w-6 items-center justify-center rounded-lg border-2',
+          [
+            active
+              ? 'border-primary-400 bg-primary-400 dark:border-primary-300 dark:bg-primary-300'
+              : 'border-gray-900 dark:border-slate-200',
+          ]
+        )}
+      >
+        <div
+          className={clsx('h-3 w-0.5 rotate-12 rounded-full', [
+            active ? 'bg-white' : 'bg-gray-900 dark:bg-slate-200',
+          ])}
+        />
+      </div>
       <div className={clsx('-mt-1 hidden text-2xl', 'md:block')}>
         <span className={clsx('text-gray-900', 'dark:text-slate-200')}>
           enji
