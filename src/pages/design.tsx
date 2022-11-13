7f0b360e9dc0f9753c704ee2472b7e0e960f21d0
enjidev <me@enji.dev>
2022-11-13 07:11:18 +0700

    feat: add base 'page is under construction'

---
+++ src/pages/design.tsx
@@ -0,0 +1,39 @@
+import clsx from 'clsx';
+import Head from 'next/head';
+
+const Design = () => {
+  return (
+    <>
+      <Head>
+        <title>Design &middot; Enji Kusnadi</title>
+        <meta name="description" content="Hi!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        className={clsx(
+          'background-grid flex h-full items-center justify-center'
+        )}
+      >
+        <div
+          className={clsx(
+            'content-wrapper text-slate-600',
+            'dark:text-slate-400'
+          )}
+        >
+          <div className={clsx('py-24 text-center')}>
+            <div
+              className={clsx('mb-3 text-6xl font-extrabold', 'md:text-9xl')}
+            >
+              OOPS
+            </div>
+            <h1 className={clsx('text-sm', 'md:text-3xl')}>
+              page is under construction
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Design;
