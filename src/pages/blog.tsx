5cbf4f550be9bdf928c38e632d5643274f7eb397
enjidev <me@enji.dev>
2022-11-14 11:29:00 +0700

    refactor: use PageHeader component

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,5 @@
-import clsx from 'clsx';
 import Head from 'next/head';
+import PageHeader from '@/components/PageHeader';
 
 const Blog = () => {
   return (
@@ -9,29 +9,7 @@ const Blog = () => {
         <meta name="description" content="Hi!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={clsx(
-          'background-grid flex h-full items-center justify-center'
-        )}
-      >
-        <div
-          className={clsx(
-            'content-wrapper text-slate-600',
-            'dark:text-slate-400'
-          )}
-        >
-          <div className={clsx('py-24 text-center')}>
-            <div
-              className={clsx('mb-3 text-6xl font-extrabold', 'md:text-9xl')}
-            >
-              OOPS
-            </div>
-            <h1 className={clsx('text-sm', 'md:text-3xl')}>
-              page is under construction
-            </h1>
-          </div>
-        </div>
-      </div>
+      <PageHeader title="Blog" desc="coming very soon!" />
     </>
   );
 };
