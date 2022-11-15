2ca03fe1930f599565f356c4bed4c7007e642ef6
enjidev <me@enji.dev>
2022-11-15 09:12:51 +0700

    feat: add ChevronRightIcon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -59,6 +59,19 @@ export const HeartIcon = (props: ComponentProps<'svg'>) => {
   );
 };
 
+export const ChevronRightIcon = (props: ComponentProps<'svg'>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M14.5 7.40192C16.5 8.55662 16.5 11.4434 14.5 12.5981L8.5 16.0622C6.5 17.2169 4 15.7735 4 13.4641L4 6.5359C4 4.2265 6.5 2.78312 8.5 3.93782L14.5 7.40192Z" />
+    </svg>
+  );
+};
+
 export const CodeIcon = (props: ComponentProps<'svg'>) => {
   return (
     <svg
