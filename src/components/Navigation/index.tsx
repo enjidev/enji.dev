fb4b5a6756dd1ce273acaa0b8594833024beacef
enjidev <me@enji.dev>
2022-11-16 11:34:19 +0700

    refactor: change content-wrapper width

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -11,64 +11,75 @@ const Navbar = () => {
   return (
     <nav
       className={clsx(
-        'relative z-50 flex items-center justify-between border-b border-divider-light bg-white px-2 py-4 text-sm',
-        'md:px-4',
+        'border-b border-divider-light bg-white',
         'dark:border-divider-dark dark:bg-slate-900'
       )}
     >
-      <div className={clsx('flex', 'md:gap-2')}>
-        <NavLogo href="/" title="Home" />
-        <ul className={clsx('flex items-center font-bold', 'md:gap-1')}>
-          <li>
-            <NavLink title="Projects" href="/projects" color="purple" />
-          </li>
-          <li>
-            <NavLinkSeparator />
-          </li>
-          <li>
-            <NavLink title="Studio" href="/studio" color="purple" />
-          </li>
-          <li>
-            <NavLinkSeparator />
-          </li>
-          <li>
-            <NavLinkDropdown
-              title="Works"
-              items={[
-                { title: 'Skills & Tools', href: '/skills' },
-                { title: 'Testimonials', href: '/testimonials' },
-                { title: 'Contact', href: '/contact' },
-                { title: 'FAQ', href: '/faq' },
-              ]}
-            />
-          </li>
-        </ul>
+      <div className={clsx('content-wrapper-max')}>
+        <div
+          className={clsx(
+            'relative z-50 flex items-center justify-between py-4 px-2 text-sm',
+            'md:px-4'
+          )}
+        >
+          <div className={clsx('flex', 'md:gap-2')}>
+            <NavLogo href="/" title="Home" />
+            <ul className={clsx('flex items-center font-bold', 'md:gap-1')}>
+              <li>
+                <NavLink title="Projects" href="/projects" color="purple" />
+              </li>
+              <li>
+                <NavLinkSeparator />
+              </li>
+              <li>
+                <NavLink title="Studio" href="/studio" color="purple" />
+              </li>
+              <li>
+                <NavLinkSeparator />
+              </li>
+              <li>
+                <NavLinkDropdown
+                  title="Works"
+                  items={[
+                    { title: 'Skills & Tools', href: '/skills' },
+                    { title: 'Testimonials', href: '/testimonials' },
+                    { title: 'Contact', href: '/contact' },
+                    { title: 'FAQ', href: '/faq' },
+                  ]}
+                />
+              </li>
+            </ul>
+          </div>
+          <ul className={clsx('flex items-center gap-2')}>
+            <li className={clsx('hidden', 'sm:block')}>
+              <NavIcon
+                href="https://github.com/enjidev"
+                icon={<GitHubIcon className={clsx('h-5 w-5')} />}
+                title="GitHub"
+              />
+            </li>
+            <li>
+              <NavIcon
+                href="https://twitter.com/enjidev"
+                icon={<TwitterIcon className={clsx('h-5 w-5')} />}
+                title="Twitter"
+                label="enjidev"
+              />
+            </li>
+            <li>
+              <div
+                className={clsx(
+                  'h-3 w-[1px] bg-slate-200',
+                  'dark:bg-slate-700'
+                )}
+              />
+            </li>
+            <li className={clsx('mr-2')}>
+              <NavIconTheme />
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className={clsx('flex items-center gap-2')}>
-        <li className={clsx('hidden', 'sm:block')}>
-          <NavIcon
-            href="https://github.com/enjidev"
-            icon={<GitHubIcon className={clsx('h-5 w-5')} />}
-            title="GitHub"
-          />
-        </li>
-        <li>
-          <NavIcon
-            href="https://twitter.com/enjidev"
-            icon={<TwitterIcon className={clsx('h-5 w-5')} />}
-            title="Twitter"
-            label="enjidev"
-          />
-        </li>
-        <li>
-          <div
-            className={clsx('h-3 w-[1px] bg-slate-200', 'dark:bg-slate-700')}
-          />
-        </li>
-        <li className={clsx('mr-2')}>
-          <NavIconTheme />
-        </li>
-      </ul>
     </nav>
   );
 };
