4aa96e95b22d2eee98396548750d9b6c827b258e
enjidev <me@enji.dev>
2022-11-16 11:06:06 +0700

    feat: add label to NavIcon

--- src/components/Navigation/NavIcon.tsx
+++ src/components/Navigation/NavIcon.tsx
@@ -4,25 +4,46 @@ interface NavIconProps {
   href: string;
   icon: React.ReactElement;
   title: string;
+  label?: string;
 }
 
-const NavIcon = ({ href, icon, title }: NavIconProps) => {
+const NavIcon = ({ href, icon, title, label }: NavIconProps) => {
   return (
     <a
       href={href}
-      className={clsx('flex items-center justify-center rounded-xl')}
+      className={clsx(
+        'flex items-center justify-center rounded-xl',
+        'hover:bg-slate-100',
+        'dark:hover:bg-slate-800/50',
+        [
+          label && [
+            'text-gray-800',
+            'sm:bg-slate-100 sm:pr-4 sm:pl-2',
+            'sm:hover:bg-slate-200',
+            'dark:text-slate-100 sm:dark:bg-slate-800/50 sm:dark:hover:bg-slate-700/50',
+          ],
+        ]
+      )}
       aria-label={title}
       target="_blank"
       rel="noreferrer"
     >
       <span
-        className={clsx(
-          'flex h-9 w-9 items-center justify-center rounded-xl text-gray-800',
-          'dark:text-slate-100'
-        )}
+        className={clsx('flex h-9 w-9 items-center justify-center rounded-xl')}
       >
         {icon}
       </span>
+      {label && (
+        <span
+          className={clsx(
+            'hidden text-xs font-bold',
+            'sm:block',
+            'dark:font-semibold'
+          )}
+        >
+          {label}
+        </span>
+      )}
     </a>
   );
 };
