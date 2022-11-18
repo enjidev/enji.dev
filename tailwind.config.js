c5e32ccd0badf8e7aa61b844967de04cf4fb174a
enjidev <me@enji.dev>
2022-11-19 05:37:04 +0700

    chore: flip vertically background-grid image

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,7 @@ module.exports = {
         {
           'bg-grid': (value) => ({
             backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}" stroke-dasharray="5 3"><path d="M0 .5H31.5V32"/></svg>`
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}" stroke-dasharray="5 3" transform="scale(1, -1)"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
         },
