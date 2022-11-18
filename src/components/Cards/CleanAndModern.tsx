8ac4d78be3b8f304ae9726f12457f49dab799809
enjidev <me@enji.dev>
2022-11-18 09:33:11 +0700

    feat: add CleanAndModern card

---
+++ src/components/Cards/CleanAndModern.tsx
@@ -0,0 +1,166 @@
+import clsx from 'clsx';
+import { CalendarIcon } from '@/components/Icons';
+
+export type CleanAndModernState = 'spacing' | 'typography' | 'colors';
+
+interface CleanAndModernProps {
+  state: Array<CleanAndModernState>;
+}
+
+const CleanAndModern = ({ state }: CleanAndModernProps) => {
+  return (
+    <div
+      className={clsx(
+        'w-full rounded-xl border transition',
+        'lg:w-96',
+        state.includes('spacing') && ['p-6'],
+        state.includes('typography') && ['text-sm'],
+        state.includes('colors')
+          ? [
+              'border-divider-light bg-white',
+              'dark:border-divider-dark dark:bg-slate-900',
+            ]
+          : ['border-black bg-white', 'dark:border-white dark:bg-[#050914]']
+      )}
+    >
+      <div
+        className={clsx(
+          'flex items-center',
+          state.includes('spacing') && ['mb-4 justify-between']
+        )}
+      >
+        <div className={clsx('flex')}>
+          <div
+            className={clsx(
+              'relative flex h-8 w-8 items-center justify-center rounded-full border-2 transition',
+              state.includes('spacing') && [''],
+              state.includes('typography') && ['font-bold'],
+              state.includes('colors')
+                ? [
+                    'border-white bg-sky-400 text-white',
+                    'dark:border-slate-900',
+                  ]
+                : [
+                    'border-white bg-[#050914] text-white',
+                    'dark:border-[#050914] dark:bg-white dark:text-black',
+                  ]
+            )}
+          >
+            E
+          </div>
+        </div>
+        <div
+          className={clsx(
+            'rounded-full transition',
+            state.includes('spacing') && ['px-2 py-0.5'],
+            state.includes('typography') && ['text-xs font-bold'],
+            state.includes('colors')
+              ? [
+                  'bg-red-100 text-red-800',
+                  'dark:bg-red-500/20 dark:text-red-300',
+                ]
+              : ['bg-[#ff0000] text-white']
+          )}
+        >
+          High
+        </div>
+      </div>
+      <div
+        className={clsx(
+          'transition',
+          state.includes('spacing') && ['mb-1'],
+          state.includes('typography') && ['text-lg font-bold'],
+          state.includes('colors')
+            ? ['text-slate-700', 'dark:text-slate-300']
+            : ['text-black', 'dark:text-white']
+        )}
+      >
+        Create Documentations
+      </div>
+      <div
+        className={clsx(
+          'transition',
+          state.includes('spacing') && ['mb-4'],
+          state.includes('typography') && [''],
+          state.includes('colors')
+            ? ['text-slate-600', 'dark:text-slate-400']
+            : ['text-black', 'dark:text-white']
+        )}
+      >
+        It is good to create early documentation for our new library.
+      </div>
+      <div
+        className={clsx(
+          'flex transition',
+          state.includes('spacing') && ['mb-6 gap-2'],
+          state.includes('typography') && ['text-xs font-bold'],
+          state.includes('colors') && ['']
+        )}
+      >
+        <div
+          className={clsx(
+            'rounded-full transition',
+            state.includes('spacing') && ['px-2 py-0.5'],
+            state.includes('typography') && [''],
+            state.includes('colors')
+              ? [
+                  'bg-blue-100 text-blue-700',
+                  'dark:bg-blue-500/20 dark:text-blue-300',
+                ]
+              : ['bg-[#0000ff] text-white']
+          )}
+        >
+          Docs
+        </div>
+        <div
+          className={clsx(
+            'rounded-full transition',
+            state.includes('spacing') && ['px-2 py-0.5'],
+            state.includes('typography') && [''],
+            state.includes('colors')
+              ? [
+                  'bg-yellow-100 text-yellow-700',
+                  'dark:bg-yellow-500/20 dark:text-yellow-300',
+                ]
+              : ['bg-[#ffff00] text-black']
+          )}
+        >
+          Support
+        </div>
+      </div>
+      <div
+        className={clsx(
+          'flex items-center transition',
+          state.includes('spacing') && ['gap-1 '],
+          state.includes('typography') && ['text-xs font-medium'],
+          state.includes('colors') && ['']
+        )}
+      >
+        <CalendarIcon
+          className={clsx(
+            'h-4 w-4 transition',
+            state.includes('spacing') && ['-mt-1'],
+            state.includes('typography') && [''],
+            state.includes('colors')
+              ? ['text-slate-400', 'dark:text-slate-600']
+              : ['h-4 w-4 text-black', 'dark:text-white']
+          )}
+        />
+        <div
+          className={clsx(
+            'transition',
+            state.includes('spacing') && [''],
+            state.includes('typography') && [''],
+            state.includes('colors')
+              ? ['text-slate-600', 'dark:text-slate-400']
+              : ['text-black', 'dark:text-white']
+          )}
+        >
+          10:00 AM &middot; Tomorrow
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CleanAndModern;
