2af230a5419a94f0fa8b27d204761a0b006d6eee
enjidev <me@enji.dev>
2022-11-26 11:15:59 +0700

    feat: add NavLinkExpanded component

---
+++ src/components/shared/Navigation/NavLinkExpanded.tsx
@@ -0,0 +1,47 @@
+import clsx from 'clsx';
+import React from 'react';
+import { NavLink } from '@/components/shared/Navigation/NavLink';
+import { ChevronRightIcon } from '@/components/shared/Icons';
+
+type NavLink = {
+  href: string;
+  title: string;
+};
+
+interface NavLinkExpandedProps {
+  title: string;
+  items: Array<NavLink>;
+}
+
+const NavLinkExpanded = ({ title, items }: NavLinkExpandedProps) => {
+  return (
+    <div className={clsx('focus-visible--secondary flex')}>
+      <div
+        className={clsx(
+          'nav-link nav-link--secondary nav-link--label pointer-events-none ml-2 mr-2'
+        )}
+      >
+        {title}
+        <ChevronRightIcon className={clsx('h-3 w-3')} />
+      </div>
+      <ul className={clsx('flex items-center')}>
+        {items.map(({ title, href }, idx) => (
+          <React.Fragment key={href}>
+            <li>
+              <NavLink title={title} href={href} color="secondary" />
+            </li>
+            {idx !== items.length - 1 && (
+              <li>
+                <div className="nav-link__separator nav-link--secondary">
+                  &middot;
+                </div>
+              </li>
+            )}
+          </React.Fragment>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NavLinkExpanded;
