bfcb5edd98b281de82340111ecd4c61256994b9c
enjidev <me@enji.dev>
2022-11-27 20:58:10 +0700

    refactor: extract mdx-contents styles to single css file

--- src/components/mdx/Heading.tsx
+++ src/components/mdx/Heading.tsx
@@ -1,5 +1,4 @@
 import { getSlug } from '@/utils/mdx';
-import clsx from 'clsx';
 
 import type { MDXComponents } from 'mdx/types';
 
@@ -7,12 +6,7 @@ export const H2: NonNullable<MDXComponents['h2']> = ({ children }) => {
   const slug = getSlug(children);
 
   return (
-    <h2
-      className={clsx('mb-2 mt-10 scroll-mt-8 text-3xl font-bold')}
-      data-ss={slug}
-      data-ss-mt={32}
-      id={slug}
-    >
+    <h2 data-ss={slug} data-ss-mt={32} id={slug}>
       {children}
     </h2>
   );
@@ -22,12 +16,7 @@ export const H3: NonNullable<MDXComponents['h3']> = ({ children }) => {
   const slug = getSlug(children);
 
   return (
-    <h3
-      className={clsx('mb-2 mt-10 scroll-mt-8 text-xl font-bold')}
-      data-ss={slug}
-      data-ss-mt={32}
-      id={slug}
-    >
+    <h3 data-ss={slug} data-ss-mt={32} id={slug}>
       {children}
     </h3>
   );
