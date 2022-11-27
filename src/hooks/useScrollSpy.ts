8d9392096a721615cc84caff26207a4662db9894
enjidev <me@enji.dev>
2022-11-27 17:02:58 +0700

    refactor: move scrollSpy function inside useEffect

--- src/hooks/useScrollSpy.ts
+++ src/hooks/useScrollSpy.ts
@@ -3,34 +3,33 @@ import { useEffect, useState } from 'react';
 export default function useScrollSpy() {
   const [currentSlug, setCurrentSlug] = useState<string | null>(null);
 
-  const scrollSpy = () => {
-    const items = document.querySelectorAll<HTMLElement>('[data-ss]');
-
-    let currentActiveSlug = currentSlug;
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i];
-      const slug = item.dataset.ss || null;
-      const offsetTop: number = Number(item.dataset.ssMt) + 5 || 0;
-
-      const { top } = item.getBoundingClientRect();
-
-      if (top - offsetTop < 0) {
-        currentActiveSlug = slug;
-      }
-    }
-
-    setCurrentSlug(currentActiveSlug);
-  };
-
   useEffect(() => {
+    const scrollSpy = () => {
+      const items = document.querySelectorAll<HTMLElement>('[data-ss]');
+
+      let currentActiveSlug = currentSlug;
+
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const slug = item.dataset.ss || null;
+        const offsetTop: number = Number(item.dataset.ssMt) + 5 || 0;
+
+        const { top } = item.getBoundingClientRect();
+
+        if (top - offsetTop < 0) {
+          currentActiveSlug = slug;
+        }
+      }
+
+      setCurrentSlug(currentActiveSlug);
+    };
+
     scrollSpy();
-
     document.addEventListener('scroll', scrollSpy);
-
     return () => {
       document.removeEventListener('scroll', scrollSpy);
     };
-  });
+  }, [currentSlug]);
 
   return currentSlug;
 }
