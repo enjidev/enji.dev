7e284fa13a3cded03ebdacfa94a3373ac6dd4162
enjidev <me@enji.dev>
2022-11-27 21:36:31 +0700

    feat: add anchor to mdx heading

--- src/components/mdx/Heading.tsx
+++ src/components/mdx/Heading.tsx
@@ -1,4 +1,6 @@
+import clsx from 'clsx';
 import { getSlug } from '@/utils/mdx';
+import { HashtagIcon } from '@/components/shared/Icons';
 
 import type { MDXComponents } from 'mdx/types';
 
@@ -6,7 +8,15 @@ export const H2: NonNullable<MDXComponents['h2']> = ({ children }) => {
   const slug = getSlug(children);
 
   return (
-    <h2 data-ss={slug} data-ss-mt={32} id={slug}>
+    <h2
+      id={slug}
+      data-ss={slug}
+      data-ss-mt={32}
+      className={clsx('with-anchor group')}
+    >
+      <a href={`#${slug}`} className={clsx('group-hover:opacity-100')}>
+        <HashtagIcon className={clsx('h-5 w-5')} />
+      </a>
       {children}
     </h2>
   );
@@ -16,7 +26,15 @@ export const H3: NonNullable<MDXComponents['h3']> = ({ children }) => {
   const slug = getSlug(children);
 
   return (
-    <h3 data-ss={slug} data-ss-mt={32} id={slug}>
+    <h3
+      id={slug}
+      data-ss={slug}
+      data-ss-mt={32}
+      className={clsx('with-anchor group')}
+    >
+      <a href={`#${slug}`} className={clsx('group-hover:opacity-100')}>
+        <HashtagIcon className={clsx('h-5 w-5')} />
+      </a>
       {children}
     </h3>
   );
