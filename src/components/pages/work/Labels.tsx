7a1a77328e3a52a6b5020bd6952353669638bf57
enjidev <me@enji.dev>
2022-11-28 18:30:51 +0700

    feat: add title prop

--- src/components/pages/work/Labels.tsx
+++ src/components/pages/work/Labels.tsx
@@ -20,15 +20,26 @@ export const Label = ({ title, indicator }: LabelProps) => {
 };
 
 interface LabelsProps {
+  title: string;
   items: Array<LabelProps>;
 }
 
-const Labels = ({ items }: LabelsProps) => {
+const Labels = ({ title, items }: LabelsProps) => {
   return (
-    <div className={clsx('flex flex-wrap gap-3')}>
-      {items.map(({ title, indicator }) => (
-        <Label key={title} title={title} indicator={indicator} />
-      ))}
+    <div
+      className={clsx(
+        'flex flex-col items-start gap-2',
+        'sm:flex-row sm:gap-6'
+      )}
+    >
+      <div className={clsx('w-48', 'sm:gap-4 sm:text-right')}>
+        <strong className={clsx('text-sm')}>{title}</strong>
+      </div>
+      <div className={clsx('flex flex-1 flex-wrap gap-2')}>
+        {items.map(({ title, indicator }) => (
+          <Label key={title} title={title} indicator={indicator} />
+        ))}
+      </div>
     </div>
   );
 };
