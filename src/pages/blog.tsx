e8a25c6ec363be2f95881131e23e08786e21d163
enjidev <me@enji.dev>
2022-11-29 12:37:28 +0700

    feat: show posts preview

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,23 +1,62 @@
+import clsx from 'clsx';
+import Link from 'next/link';
 import Head from '@/components/meta/Head';
-import PageHeader from '@/components/shared/Header/PageHeader';
+import BlogHeader from '@/components/shared/Header/BlogHeader';
 import { getSortedPostsData } from '@/lib/posts';
+import { ChevronRightIcon } from '@/components/shared/Icons';
 
 import type { GetStaticProps, NextPage } from 'next';
 import type { TPostFrontMatter } from '@/types';
-
+import PostPreview from '@/components/pages/blog/PostPreview';
 interface BlogProps {
   posts: Array<TPostFrontMatter>;
 }
 
-const Blog: NextPage<BlogProps> = () => {
+const Blog: NextPage<BlogProps> = ({ posts }) => {
   return (
     <>
       <Head title="Blog" description="Blog" />
-      <PageHeader
-        title="Blog"
-        description="— page is coming very soon!"
-        caption="Personal"
+      <BlogHeader
+        title="Personal Blog"
+        description="All about the detailed tutorials, stories, and tech-related stuff."
       />
+      <div className={clsx('content-wrapper')}>
+        <div
+          className={clsx(
+            'flex flex-col gap-8',
+            'md:flex-row md:gap-8 lg:gap-24'
+          )}
+        >
+          <div className={clsx('md:w-64')}>{/* TODO: Filter Posts */}</div>
+          <div className={clsx('flex-1')}>
+            {posts.map(({ slug, title, date, description }) => (
+              <div
+                key={slug}
+                className={clsx(
+                  'mb-8 flex items-start gap-4',
+                  'md:mb-4 md:gap-6'
+                )}
+              >
+                <div
+                  className={clsx(
+                    'mt-14 w-8 -translate-y-1 border-b border-divider-light',
+                    'md:mt-16 md:w-20',
+                    'dark:border-divider-dark'
+                  )}
+                />
+                <div className={clsx('flex-1')}>
+                  <PostPreview
+                    title={title}
+                    description={description}
+                    date={date}
+                    slug={slug}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
