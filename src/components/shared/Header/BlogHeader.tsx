ad560813c82c9f6e29602ab7989bf4114a1e68e4
enjidev <me@enji.dev>
2022-11-29 12:36:15 +0700

    feat: add BlogHeader component

---
+++ src/components/shared/Header/BlogHeader.tsx
@@ -0,0 +1,77 @@
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+
+const animation = {
+  hide: { x: -32, opacity: 0 },
+  show: {
+    x: 0,
+    opacity: 1,
+  },
+};
+
+interface BlogHeaderProps {
+  title: string;
+  description?: string;
+  status?: string;
+}
+
+const BlogHeader = ({ title, description, status }: BlogHeaderProps) => {
+  return (
+    <header
+      id="page-header"
+      className={clsx(
+        'background-grid background-grid--fade-out z-[900] mb-10 border-b border-divider-light pt-36 pb-6',
+        'md:mb-0 md:border-none md:pb-20 md:pt-52',
+        'dark:border-divider-dark'
+      )}
+    >
+      <div className={clsx('content-wrapper text-center')}>
+        <motion.div
+          initial={animation.hide}
+          animate={animation.show}
+          transition={{ delay: 0.1 }}
+        >
+          <h1
+            className={clsx(
+              'text-[2.5rem] font-extrabold leading-none text-slate-700',
+              'md:text-7xl',
+              'dark:text-slate-300'
+            )}
+          >
+            {title}
+            {status && (
+              <span
+                className={clsx(
+                  'ml-2 align-top text-base font-bold text-slate-400',
+                  'md:ml-4 md:text-2xl',
+                  'dark:text-slate-600'
+                )}
+              >
+                {status}
+              </span>
+            )}
+          </h1>
+        </motion.div>
+        {description && (
+          <motion.div
+            initial={animation.hide}
+            animate={animation.show}
+            transition={{ delay: 0.2 }}
+          >
+            <p
+              className={clsx(
+                'mx-auto mt-2 text-center text-lg text-slate-600',
+                'md:mt-6 md:text-2xl lg:max-w-[500px] xl:max-w-[700px]',
+                'dark:text-slate-400'
+              )}
+            >
+              {description}
+            </p>
+          </motion.div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default BlogHeader;
