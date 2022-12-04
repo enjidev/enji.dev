6a4d147b69a921082bc7940b14aa98980d168e4f
enjidev <me@enji.dev>
2022-12-04 23:58:17 +0700

    feat: add children property to Skeleton component

--- src/components/shared/Wireframes/Skeletons.tsx
+++ src/components/shared/Wireframes/Skeletons.tsx
@@ -1,17 +1,37 @@
 import clsx from 'clsx';
 
-interface SkeletonSmallProps {
+interface SkeletonProps {
   /**
    * Skeleton width in pixels.
    */
   w?: number;
+  children?: React.ReactNode;
 }
 
-export const SkeletonSmall = ({ w = 16 }: SkeletonSmallProps) => {
+export const SkeletonSm = ({ w = 16, children }: SkeletonProps) => {
   return (
     <div
-      className={clsx('h-4 rounded-md bg-slate-200/80', 'dark:bg-slate-100/5')}
+      className={clsx(
+        'flex h-4 items-center rounded-md bg-slate-200/80',
+        'dark:bg-slate-100/5'
+      )}
       style={{ width: w }}
-    />
+    >
+      {children}
+    </div>
+  );
+};
+
+export const SkeletonMd = ({ w = 24, children }: SkeletonProps) => {
+  return (
+    <div
+      className={clsx(
+        'flex h-6 items-center rounded-lg bg-slate-200/80',
+        'dark:bg-slate-100/5'
+      )}
+      style={{ width: w }}
+    >
+      {children}
+    </div>
   );
 };
