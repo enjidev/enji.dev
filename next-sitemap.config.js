671b80d8740920db37a4f22857361fbbdb8005a0
enjidev <me@enji.dev>
2022-12-16 04:28:33 +0700

    feat: integrate with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.enji.dev',
+  priority: 0.6,
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  transform: async (config, path) => {
+    const defaultValue = (priority) => {
+      return {
+        loc: path,
+        changefreq: config.changefreq,
+        priority: priority || config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      };
+    };
+
+    if (path === '/') {
+      return defaultValue(1.0);
+    }
+
+    if (path === '/blog') {
+      return defaultValue(0.9);
+    }
+
+    if (path === '/projects') {
+      return defaultValue(0.9);
+    }
+
+    if (path.indexOf('/work') === 0) {
+      return defaultValue(0.8);
+    }
+
+    return defaultValue();
+  },
+};
