2d995ec6188fad90158a5aee44155781e233467a
enjidev <me@enji.dev>
2022-12-16 08:26:13 +0700

    chore: use article element

--- src/components/pages/blog/PostPreview.tsx
+++ src/components/pages/blog/PostPreview.tsx
@@ -19,49 +19,54 @@ function PostPreview({
   lang,
 }: PostPreviewProps) {
   return (
-    <Link
-      key={slug}
-      href={`blog/${slug}`}
-      className={clsx('group block rounded-xl bg-gradient-to-t p-4', 'md:p-6')}
-    >
-      <div
+    <article lang={lang}>
+      <Link
+        key={slug}
+        href={`blog/${slug}`}
         className={clsx(
-          'text-slate mb-2 flex gap-1 text-xs text-slate-500',
-          'dark:text-slate-400'
+          'group block rounded-xl bg-gradient-to-t p-4',
+          'md:p-6'
         )}
       >
-        <span>{formatDate(date)}</span>
-        <span>&middot;</span>
-        <span>{formatLang(lang)}</span>
-      </div>
-      <div className={clsx('mb-1')}>
-        <h2
+        <div
           className={clsx(
-            'font-extrabold text-slate-700',
-            'dark:text-slate-300'
+            'text-slate mb-2 flex gap-1 text-xs text-slate-500',
+            'dark:text-slate-400'
           )}
         >
-          {title}
-        </h2>
-      </div>
-      <p
-        className={clsx(
-          'mb-2 block leading-relaxed text-slate-600',
-          'dark:text-slate-400'
-        )}
-      >
-        {description}
-      </p>
-      <div
-        className={clsx(
-          'flex items-center gap-1 text-sm font-semibold text-accent-600',
-          'dark:text-accent-400'
-        )}
-      >
-        read more{' '}
-        <ChevronRightIcon className="mt-1 h-3 w-3 transition group-hover:animate-bounce-x" />
-      </div>
-    </Link>
+          <time dateTime={date}>{formatDate(date)}</time>
+          <span>&middot;</span>
+          <span>{formatLang(lang)}</span>
+        </div>
+        <div className={clsx('mb-1')}>
+          <h2
+            className={clsx(
+              'font-extrabold text-slate-700',
+              'dark:text-slate-300'
+            )}
+          >
+            {title}
+          </h2>
+        </div>
+        <p
+          className={clsx(
+            'mb-2 block leading-relaxed text-slate-600',
+            'dark:text-slate-400'
+          )}
+        >
+          {description}
+        </p>
+        <div
+          className={clsx(
+            'flex items-center gap-1 text-sm font-semibold text-accent-600',
+            'dark:text-accent-400'
+          )}
+        >
+          read more{' '}
+          <ChevronRightIcon className="mt-1 h-3 w-3 transition group-hover:animate-bounce-x" />
+        </div>
+      </Link>
+    </article>
   );
 }
 
