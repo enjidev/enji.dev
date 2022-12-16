e3ce9ff04abb8b45ecb4a5858a1734a77c6e7572
enjidev <me@enji.dev>
2022-12-16 17:04:43 +0700

    chore: wrap navigation with header element

--- src/components/shared/Navigation/index.tsx
+++ src/components/shared/Navigation/index.tsx
@@ -23,90 +23,91 @@ function Navbar() {
   const isExit = useOnExit('#page-header');
 
   return (
-    <nav
-      aria-label="Site Navigation"
+    <header
       className={clsx('fixed right-0 left-0 z-[1000] transition', [
         position > 0 ? 'translate-y-0' : 'translate-y-2',
       ])}
     >
-      <div
-        className={clsx(
-          'pointer-events-none fixed top-0 left-0 right-0 h-16 border-b border-divider-light backdrop-blur transition',
-          'dark:border-divider-dark',
-          [
-            !isExit
-              ? ['bg-slate-100/80', 'dark:bg-[#0c1222]/80']
-              : ['bg-white/70', 'dark:bg-slate-900/80'],
-          ],
-          [
-            position > 0
-              ? 'translate-y-0 opacity-100'
-              : 'translate-y-2 opacity-0',
-          ]
-        )}
-      />
-      <div className={clsx('content-wrapper-max')}>
+      <nav aria-label="Site Navigation" role="navigation">
         <div
           className={clsx(
-            'relative z-50 flex h-16 items-center justify-between px-2 text-sm',
-            'md:px-4'
+            'pointer-events-none fixed top-0 left-0 right-0 h-16 border-b border-divider-light backdrop-blur transition',
+            'dark:border-divider-dark',
+            [
+              !isExit
+                ? ['bg-slate-100/80', 'dark:bg-[#0c1222]/80']
+                : ['bg-white/70', 'dark:bg-slate-900/80'],
+            ],
+            [
+              position > 0
+                ? 'translate-y-0 opacity-100'
+                : 'translate-y-2 opacity-0',
+            ]
           )}
-        >
-          <div className={clsx('flex', 'md:gap-2')} data-accent="violet">
-            <NavLogo href="/" title="Home" />
-            <ul className={clsx('flex items-center', 'md:gap-1')}>
-              <li>
-                <NavLink title="Projects" href="/projects" />
+        />
+        <div className={clsx('content-wrapper-max')}>
+          <div
+            className={clsx(
+              'relative z-50 flex h-16 items-center justify-between px-2 text-sm',
+              'md:px-4'
+            )}
+          >
+            <div className={clsx('flex', 'md:gap-2')} data-accent="violet">
+              <NavLogo href="/" title="Home" />
+              <ul className={clsx('flex items-center', 'md:gap-1')}>
+                <li>
+                  <NavLink title="Projects" href="/projects" />
+                </li>
+                <li>
+                  <NavLink title="Blog" href="/blog" />
+                </li>
+                <li className={clsx('lg:hidden')} data-accent="blue">
+                  <NavLinkDropdown title="Work" items={workLinks} />
+                </li>
+                <li className={clsx('hidden lg:block')} data-accent="blue">
+                  <NavLinkExpanded title="Work" items={workLinks} />
+                </li>
+              </ul>
+            </div>
+            <ul className={clsx('flex items-center gap-2')}>
+              <li className={clsx('-mr-1 hidden', 'sm:block')}>
+                <NavIcon
+                  href="https://figma.com/@enjidev"
+                  icon={<FigmaIcon className={clsx('h-5 w-5')} />}
+                  title="Figma"
+                />
+              </li>
+              <li className={clsx('hidden', 'sm:block')}>
+                <NavIcon
+                  href="https://github.com/enjidev"
+                  icon={<GitHubIcon className={clsx('h-5 w-5')} />}
+                  title="GitHub"
+                />
               </li>
               <li>
-                <NavLink title="Blog" href="/blog" />
+                <NavIcon
+                  href="https://twitter.com/enjidev"
+                  icon={<TwitterIcon className={clsx('h-5 w-5')} />}
+                  title="Twitter"
+                  label="enjidev"
+                />
               </li>
-              <li className={clsx('lg:hidden')} data-accent="blue">
-                <NavLinkDropdown title="Work" items={workLinks} />
+              <li>
+                <div
+                  className={clsx(
+                    'h-3 w-[1px] bg-slate-200',
+                    'dark:bg-slate-700'
+                  )}
+                />
               </li>
-              <li className={clsx('hidden lg:block')} data-accent="blue">
-                <NavLinkExpanded title="Work" items={workLinks} />
+              <li className={clsx('mr-2')}>
+                <NavIconTheme />
               </li>
             </ul>
           </div>
-          <ul className={clsx('flex items-center gap-2')}>
-            <li className={clsx('-mr-1 hidden', 'sm:block')}>
-              <NavIcon
-                href="https://figma.com/@enjidev"
-                icon={<FigmaIcon className={clsx('h-5 w-5')} />}
-                title="Figma"
-              />
-            </li>
-            <li className={clsx('hidden', 'sm:block')}>
-              <NavIcon
-                href="https://github.com/enjidev"
-                icon={<GitHubIcon className={clsx('h-5 w-5')} />}
-                title="GitHub"
-              />
-            </li>
-            <li>
-              <NavIcon
-                href="https://twitter.com/enjidev"
-                icon={<TwitterIcon className={clsx('h-5 w-5')} />}
-                title="Twitter"
-                label="enjidev"
-              />
-            </li>
-            <li>
-              <div
-                className={clsx(
-                  'h-3 w-[1px] bg-slate-200',
-                  'dark:bg-slate-700'
-                )}
-              />
-            </li>
-            <li className={clsx('mr-2')}>
-              <NavIconTheme />
-            </li>
-          </ul>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
 
