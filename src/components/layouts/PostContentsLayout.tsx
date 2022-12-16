d08a17aae056e0f80b06ad964691816c097173cf
enjidev <me@enji.dev>
2022-12-16 17:14:08 +0700

    refactor: remove article on post layout

--- src/components/layouts/PostContentsLayout.tsx
+++ src/components/layouts/PostContentsLayout.tsx
@@ -42,31 +42,29 @@ function PostContentsLayout({
         ogImage={ogImage.default}
         structuredData={structuredData}
       />
-      <article lang={lang}>
-        <PageHeader
-          title={title}
-          caption={category}
-          description={description}
-        />
-        <div className={clsx('content-wrapper')}>
-          <div className={clsx('flex gap-8', 'xl:gap-24')}>
-            <div
-              className={clsx(
-                'hidden border-l border-divider-light',
-                ' dark:border-divider-dark lg:block'
-              )}
-            />
-            <div className={clsx('mdx-contents flex-1')} id="mdx-contents">
-              {children}
-            </div>
-            {tableOfContents.length > 0 && (
-              <div className={clsx('-mt-36 hidden', 'lg:block')}>
-                <TableOfContents items={tableOfContents} />
-              </div>
+      <PageHeader title={title} caption={category} description={description} />
+      <div className={clsx('content-wrapper')}>
+        <div className={clsx('flex gap-8', 'xl:gap-24')}>
+          <div
+            className={clsx(
+              'hidden border-l border-divider-light',
+              ' dark:border-divider-dark lg:block'
             )}
+          />
+          <div
+            className={clsx('mdx-contents flex-1')}
+            id="mdx-contents"
+            lang={lang}
+          >
+            {children}
           </div>
+          {tableOfContents.length > 0 && (
+            <div className={clsx('-mt-36 hidden', 'lg:block')}>
+              <TableOfContents items={tableOfContents} />
+            </div>
+          )}
         </div>
-      </article>
+      </div>
     </>
   );
 }
