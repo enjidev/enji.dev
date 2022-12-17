8b4b584c61e9751e83b11d8f94776be3b014afd6
enjidev <me@enji.dev>
2022-12-17 08:34:51 +0700

    chore: improve layout and add homepage link

--- src/contents/404.tsx
+++ src/contents/404.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 
 function Error404Contents() {
   return (
@@ -9,16 +10,23 @@ function Error404Contents() {
     >
       <div
         className={clsx(
-          'content-wrapper text-slate-600',
+          'content-wrapper text-center text-slate-600',
           'dark:text-slate-400'
         )}
       >
-        <div className={clsx('py-24 text-center')}>
-          <div className={clsx('mb-3 text-6xl font-extrabold', 'md:text-9xl')}>
-            404
-          </div>
-          <h1 className={clsx('text-sm', 'md:text-3xl')}>Page Not Found</h1>
-        </div>
+        <h1 className={clsx('py-12 text-center')}>
+          <div className={clsx('mb-3 text-8xl font-extrabold')}>404</div>
+          <div className={clsx('text-2xl')}> Page Not Found </div>
+        </h1>
+        <Link
+          href="/"
+          className={clsx(
+            'rounded-xl py-2 px-4 text-xl text-accent-600',
+            'dark:text-accent-400'
+          )}
+        >
+          back to homepage
+        </Link>
       </div>
     </div>
   );
