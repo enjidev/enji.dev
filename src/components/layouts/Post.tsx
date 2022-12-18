a1151ef9db305a8ca8ffca960eb6b690f05f0724
enjidev <me@enji.dev>
2022-12-18 09:48:12 +0700

    refactor: add WithTableOfContents component

--- src/components/layouts/Post.tsx
+++ src/components/layouts/Post.tsx
@@ -1,9 +1,7 @@
-import clsx from 'clsx';
-
+import WithTableOfContents from '@/components/layouts/WithTableOfContents';
 import Head from '@/components/meta/Head';
 import PageHeader from '@/components/shared/Header/PageHeader';
 import SkipNavigation from '@/components/shared/Navigation/SkipNavigation';
-import TableOfContents from '@/components/shared/TableOfContents';
 
 import { getPostOgImageUrl, getPostStructuredData } from '@/helpers/post';
 
@@ -47,26 +45,9 @@ function Post({
       />
       <SkipNavigation />
       <PageHeader title={title} description={description} caption={caption} />
-      <div className={clsx('content-wrapper')}>
-        <div className={clsx('flex flex-row-reverse gap-8', 'xl:gap-24')}>
-          <div className={clsx('-mt-36 hidden', 'lg:block')}>
-            <TableOfContents items={tableOfContents} />
-          </div>
-          <div
-            className={clsx('mdx-contents flex-1 scroll-mt-[86px]')}
-            id="main-contents"
-            data-ss-wrapper
-          >
-            {children}
-          </div>
-          <div
-            className={clsx(
-              'hidden border-l border-divider-light',
-              ' dark:border-divider-dark lg:block'
-            )}
-          />
-        </div>
-      </div>
+      <WithTableOfContents tableOfContents={tableOfContents}>
+        {children}
+      </WithTableOfContents>
     </>
   );
 }
---
+++ src/components/layouts/WithTableOfContents.tsx
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+
+import TableOfContents from '@/components/shared/TableOfContents';
+
+import type { TTableOfContents } from '@/types';
+
+interface PageWithMDXProps {
+  tableOfContents: TTableOfContents;
+  children: React.ReactNode;
+}
+
+function PageWithMDX({ tableOfContents, children }: PageWithMDXProps) {
+  return (
+    <div className={clsx('content-wrapper')}>
+      <div className={clsx('flex flex-row-reverse gap-8', 'xl:gap-24')}>
+        <div className={clsx('-mt-36 hidden', 'lg:block')}>
+          <TableOfContents items={tableOfContents} />
+        </div>
+        <div
+          className={clsx('mdx-contents flex-1 scroll-mt-[86px]')}
+          id="main-contents"
+          data-ss-wrapper
+        >
+          {children}
+        </div>
+        <div
+          className={clsx(
+            'hidden border-l border-divider-light',
+            ' dark:border-divider-dark lg:block'
+          )}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PageWithMDX;
