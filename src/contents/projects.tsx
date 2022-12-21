0d9944301412aab032d73bef356344e96acfde9c
enjidev <me@enji.dev>
2022-12-21 07:41:43 +0700

    feat: add npm to tailwindcss-accent project section

--- src/contents/projects.tsx
+++ src/contents/projects.tsx
@@ -1,12 +1,17 @@
 import clsx from 'clsx';
+import { useState } from 'react';
 
 import { GitHubIcon, NpmIcon } from '@/components/shared/Icons';
+import { SectionButton } from '@/components/shared/SectionButton';
 import SectionContent from '@/components/shared/SectionContent';
 import SectionTitle from '@/components/shared/SectionTitle';
 import AppWindow from '@/components/shared/Wireframes/AppWindow';
 import GitHubWireframe from '@/components/shared/Wireframes/GitHub';
+import NpmWireframe from '@/components/shared/Wireframes/Npm';
 
 function ProjectsContents() {
+  const [currentState, setCurrentState] = useState<'npm' | 'github'>('github');
+
   return (
     <>
       <div className={clsx('mt-20')} />
@@ -21,7 +26,24 @@ function ProjectsContents() {
       />
       <SectionContent>
         <div className={clsx('flex', 'lg:gap-12')}>
-          <div className={clsx('hidden flex-1', 'lg:flex')} />
+          <div className={clsx('hidden flex-1 flex-col gap-3 pt-8', 'lg:flex')}>
+            <div className={clsx('flex flex-col gap-3')}>
+              <SectionButton
+                title="Available on GitHub"
+                icon={<GitHubIcon className={clsx('my-2 h-16 w-16')} />}
+                description="Access powerful and flexible package on GitHub with MIT license."
+                active={currentState === 'github'}
+                onClick={() => setCurrentState('github')}
+              />
+              <SectionButton
+                title="npm package"
+                icon={<NpmIcon className={clsx('my-2 h-16 w-16')} />}
+                description="Install and use the package with ease thanks to its typed options."
+                active={currentState === 'npm'}
+                onClick={() => setCurrentState('npm')}
+              />
+            </div>
+          </div>
           <div className={clsx('w-full', 'lg:w-auto')}>
             <div className={clsx('-mt-[41px]')}>
               <div className={clsx('w-full', 'lg:h-[400px] lg:w-[600px]')}>
@@ -31,20 +53,30 @@ function ProjectsContents() {
                     {
                       icon: <GitHubIcon className="h-4 w-4" />,
                       title: 'enjidev/tailwindcss-accent - GitHub',
-                      isActive: true,
+                      isActive: currentState === 'github',
                     },
                     {
                       icon: <NpmIcon className="h-4 w-4" />,
                       title: 'tailwindcss-accent - npm',
-                      isActive: false,
+                      isActive: currentState === 'npm',
                     },
                   ]}
                 >
-                  <GitHubWireframe
-                    author="enjidev"
-                    repository="tailwindcss-accent"
-                    description="Add dynamic accent color to your Tailwind CSS project."
-                  />
+                  {currentState === 'github' && (
+                    <GitHubWireframe
+                      author="enjidev"
+                      license="MIT"
+                      repository="tailwindcss-accent"
+                      description="Add dynamic accent color to your Tailwind CSS project."
+                    />
+                  )}
+                  {currentState === 'npm' && (
+                    <NpmWireframe
+                      packageName="tailwindcss-accent"
+                      description="Add dynamic accent color to your Tailwind CSS project."
+                      isWithTypeScript
+                    />
+                  )}
                 </AppWindow>
               </div>
             </div>
