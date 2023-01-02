bbd8a04abbeeb5b5622afebb935630238c9e60e8
enjidev <me@enji.dev>
2023-01-03 00:53:52 +0700

    feat(mdx): add table component

--- apps/enji.dev/src/components/mdx/index.tsx
+++ apps/enji.dev/src/components/mdx/index.tsx
@@ -1,5 +1,6 @@
 import { H2, H3 } from '@/components/mdx/Heading';
 import { Link } from '@/components/mdx/Link';
+import { Table } from '@/components/mdx/Table';
 
 import type { MDXComponents } from 'mdx/types';
 
@@ -7,6 +8,7 @@ const components: MDXComponents = {
   a: Link,
   h2: H2,
   h3: H3,
+  table: Table,
 };
 
 export default components;
