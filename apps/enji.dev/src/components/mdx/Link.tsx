9cf1c7a6abb0b99f6cfb31438f15581ac201bc44
enjidev <me@enji.dev>
2023-01-04 03:33:07 +0700

    feat: add mail url type

--- apps/enji.dev/src/components/mdx/Link.tsx
+++ apps/enji.dev/src/components/mdx/Link.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import NextLink from 'next/link';
 
-import { ExternalLink } from '@/components/shared/Icons';
+import { ExternalLink, MailIcon } from '@/components/shared/Icons';
 
 import type { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
 
@@ -10,21 +10,50 @@ type Props = DetailedHTMLProps<
   HTMLAnchorElement
 >;
 
-const isExternal = (url: string) => url.indexOf('/') !== 0;
+const urlType = (url: string) => {
+  if (url.indexOf('/') === 0) {
+    return 'internal';
+  }
+
+  if (url.indexOf('mailto') === 0) {
+    return 'mail';
+  }
+
+  return 'external';
+};
 
 export function Link({ children, href }: Props) {
   if (!href) {
     return <span>{children}</span>;
   }
 
-  return isExternal(href) ? (
-    <a href={href} target="_blank" rel="noreferrer" className={clsx('link')}>
-      {children}
-      <ExternalLink className="h-3.5 w-3.5" />
-    </a>
-  ) : (
-    <NextLink href={href} className={clsx('link')}>
-      {children}
-    </NextLink>
-  );
+  const type = urlType(href);
+
+  switch (type) {
+    case 'external':
+      return (
+        <a
+          href={href}
+          target="_blank"
+          rel="noreferrer"
+          className={clsx('link')}
+        >
+          {children}
+          <ExternalLink className="h-3.5 w-3.5" />
+        </a>
+      );
+    case 'mail':
+      return (
+        <a href={href} className={clsx('link')}>
+          <MailIcon className="h-3.5 w-3.5" />
+          {children}
+        </a>
+      );
+    default:
+      return (
+        <NextLink href={href} className={clsx('link')}>
+          {children}
+        </NextLink>
+      );
+  }
 }
