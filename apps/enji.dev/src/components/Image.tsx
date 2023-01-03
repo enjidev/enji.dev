5394138777320e1c691b8c7336edac99529e1d74
enjidev <me@enji.dev>
2023-01-03 09:45:08 +0700

    chore: ability to add/change classes

--- apps/enji.dev/src/components/Image.tsx
+++ apps/enji.dev/src/components/Image.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/jsx-props-no-spreading */
-
 import clsx from 'clsx';
 import NextImage from 'next/image';
 
@@ -9,25 +8,32 @@ export type ImageProps = NextImageProps & {
   immersive?: boolean;
 };
 
-export default function Image({ immersive = true, src, ...props }: ImageProps) {
+export default function Image({
+  immersive = true,
+  className,
+  src,
+  ...props
+}: ImageProps) {
   return (
     <div className={clsx('custom-image relative')}>
       {immersive ? (
         <div
           style={{ backgroundImage: `url(${src})` }}
           className={clsx(
-            'absolute -inset-8 z-[-1] hidden rounded-[20%] bg-[length:180%_180%] bg-center opacity-50 blur-2xl',
+            'absolute -inset-8 z-[-1] rounded-[20%] bg-[length:180%_180%] bg-center opacity-50 blur-2xl',
+            'hidden', // TODO: enable and improve immersive on light mode
             'dark:block dark:opacity-25'
           )}
         />
       ) : null}
       <NextImage
         src={src}
-        {...props}
         className={clsx(
           'border-divider-light rounded-lg border',
-          'dark:border-divider-dark'
+          'dark:border-divider-dark',
+          className
         )}
+        {...props}
       />
     </div>
   );
