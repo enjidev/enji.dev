a8c1266b5061c369f68b9e59e986d1fcaa0e8ba3
enjidev <me@enji.dev>
2023-01-06 16:43:16 +0700

    feat: add custom hr component

--- apps/enji.dev/src/components/mdx/index.tsx
+++ apps/enji.dev/src/components/mdx/index.tsx
@@ -1,4 +1,5 @@
 import { H2, H3 } from '@/components/mdx/Heading';
+import { Hr } from '@/components/mdx/Hr';
 import { Link } from '@/components/mdx/Link';
 import { Pre } from '@/components/mdx/Pre';
 import { Table } from '@/components/mdx/Table';
@@ -9,6 +10,7 @@ const components: MDXComponents = {
   a: Link,
   h2: H2,
   h3: H3,
+  hr: Hr,
   pre: Pre,
   table: Table,
 };
