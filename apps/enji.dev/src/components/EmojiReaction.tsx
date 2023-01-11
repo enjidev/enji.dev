3bf76f13bdc28ef22615022deab0cd4d36b2dc51
enjidev <me@enji.dev>
2023-01-11 14:54:18 +0700

    chore: preload animated image

--- apps/enji.dev/src/components/EmojiReaction.tsx
+++ apps/enji.dev/src/components/EmojiReaction.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import { m } from 'framer-motion';
+import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
 
@@ -69,68 +70,74 @@ function EmojiReaction({
   const [src, setSrc] = useState<string>(defaultImage);
 
   return (
-    <m.button
-      title={title}
-      aria-label={title}
-      className="relative cursor-pointer select-none"
-      whileTap="tap"
-      whileHover="hover"
-      onHoverStart={() => {
-        setSrc(animatedImage);
-      }}
-      onHoverEnd={() => {
-        setSrc(defaultImage);
-      }}
-      onClick={() => {
-        setHistory((current) => [...current, getRandomAnimationValue()]);
+    <>
+      <Head>
+        <link rel="preload" as="image" href={defaultImage} />
+        <link rel="preload" as="image" href={animatedImage} />
+      </Head>
+      <m.button
+        title={title}
+        aria-label={title}
+        className="relative cursor-pointer select-none"
+        whileTap="tap"
+        whileHover="hover"
+        onHoverStart={() => {
+          setSrc(animatedImage);
+        }}
+        onHoverEnd={() => {
+          setSrc(defaultImage);
+        }}
+        onClick={() => {
+          setHistory((current) => [...current, getRandomAnimationValue()]);
 
-        onClick();
-      }}
-    >
-      {history.map(({ x, y, duration, key }) => (
-        <m.div
-          key={key}
-          className="pointer-events-none absolute h-10 w-10 select-none"
-          initial={{ y: 0, x: 0, opacity: 1 }}
-          animate={{
-            x,
-            y,
-            // fade out, start from the middle of animation
-            opacity: [1, 1, 0],
-          }}
-          transition={{
-            ease: 'easeOut',
-            duration,
-          }}
-          onAnimationComplete={() => {
-            // cleanup: remove from DOM
-            setHistory((current) => current.filter((el) => el.key !== key));
-          }}
-        >
+          onClick();
+        }}
+      >
+        {history.map(({ x, y, duration, key }) => (
+          <m.div
+            key={key}
+            className="pointer-events-none absolute h-10 w-10 select-none"
+            initial={{ y: 0, x: 0, opacity: 1 }}
+            animate={{
+              x,
+              y,
+              // fade out, start from the middle of animation
+              opacity: [1, 1, 0],
+            }}
+            transition={{
+              ease: 'easeOut',
+              duration,
+            }}
+            onAnimationComplete={() => {
+              // cleanup: remove from DOM
+              setHistory((current) => current.filter((el) => el.key !== key));
+            }}
+          >
+            <Image
+              className={clsx('h-full w-full')}
+              alt={title}
+              src={animatedImage}
+              width={48}
+              height={48}
+              unoptimized
+              priority
+            />
+          </m.div>
+        ))}
+
+        <m.div className={clsx('h-10 w-10')} variants={emojiMotion}>
           <Image
-            className={clsx('h-full w-full')}
+            className={clsx('pointer-events-none h-full w-full')}
             alt={title}
-            src={animatedImage}
+            src={src}
             width={48}
             height={48}
             unoptimized
             priority
           />
         </m.div>
-      ))}
-
-      <m.div className={clsx('h-10 w-10')} variants={emojiMotion}>
-        <Image
-          className={clsx('pointer-events-none h-full w-full')}
-          alt={title}
-          src={src}
-          width={48}
-          height={48}
-          unoptimized
-          priority
-        />
-      </m.div>
-    </m.button>
+      </m.button>
+    </>
   );
 }
 
