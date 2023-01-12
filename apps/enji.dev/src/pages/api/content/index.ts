c0862c5e20884ce78bd10e2051a4832a03012f81
enjidev <me@enji.dev>
2023-01-12 17:47:10 +0700

    chore: log api errors

--- apps/enji.dev/src/pages/api/content/index.ts
+++ apps/enji.dev/src/pages/api/content/index.ts
@@ -16,6 +16,9 @@ export default async function handler(
       res.status(405).json({ message: 'Method Not Allowed' });
     }
   } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+
     res.status(500).json({ message: 'Internal Server Error' });
   }
 }
