b3084584884fc12a981b5a828bde7e0ea2725cd5
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-12-05 06:18:00 +0200

    Extended `ProjectsContents` to be a dynamic component (initial version)

--- apps/enji.dev/src/contents/projects/index.tsx
+++ apps/enji.dev/src/contents/projects/index.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { useState } from 'react';
 
-import { ExternalLink, GitHubIcon, NpmIcon, WebIcon } from '@/components/Icons';
+import { GitHubIcon, NpmIcon, WebIcon } from '@/components/Icons';
 import { SectionButton } from '@/components/sections/SectionButton';
 import SectionContent from '@/components/sections/SectionContent';
 import SectionTitle from '@/components/sections/SectionTitle';
@@ -10,45 +10,83 @@ import AppWindow from '@/components/wireframes/AppWindow';
 import GitHubWireframe from '@/components/wireframes/GitHub';
 import NpmWireframe from '@/components/wireframes/Npm';
 
-function ProjectsContents() {
-  const [currentState, setCurrentState] = useState<'npm' | 'github' | 'web'>('github');
+type TState = 'npm' | 'github' | 'web';
 
+type TProjectsContentsProps = {
+  sectionTitle: {
+    title: string;
+    caption: string;
+    description: string;
+    button: {
+      title: string;
+      href: string;
+    };
+  };
+  availableStates: TState[];
+  appWindows: {
+    [key in TState]?: {
+      [dynamicKey: string]: string;
+    };
+  }
+};
+
+function ProjectsContents({ sectionTitle, availableStates, appWindows }: TProjectsContentsProps) {
+  const [currentState, setCurrentState] = useState<TState>('github');
+  
+  const isAvailable = (state: TState) => availableStates.includes(state);
+
+  const getBrowserTabs = () => {
+    const tabs = [];
+  
+    const addTab = (service, icon, title, isActive) => {
+      if (isAvailable(service)) {
+        tabs.push({ icon, title, isActive});
+      }
+    };
+  
+    addTab('github', <GitHubIcon className="h-4 w-4" />, `enjidev/${appWindows.github.repository} - GitHub`, true);
+    addTab('npm', <NpmIcon className="h-4 w-4" />, `${appWindows.npm.packageName} - npm `, false);
+    addTab('web', <WebIcon className="h-4 w-4" />, `${appWindows.web.alt}`, false);
+  
+    return tabs;
+  }
+  
   return (
     <>
       <SectionTitle
-        title="The dynamic accent colors."
-        caption="tailwindcss-accent"
-        description="Add accent colors for dynamic, flexible color use in your Tailwind CSS project."
+        title={sectionTitle.title}
+        caption={sectionTitle.caption}
+        description={sectionTitle.description}
         button={{
-          title: 'learn more',
-          href: '/docs/tailwindcss-accent',
+          title: sectionTitle.button.title,
+          href: sectionTitle.button.href,
         }}
       />
       <SectionContent>
         <div className={clsx('flex', 'lg:gap-12')}>
           <div className={clsx('hidden flex-1 flex-col gap-3 pt-8', 'lg:flex')}>
             <div className={clsx('flex flex-col gap-3')}>
-              <SectionButton
+              {isAvailable("github") && <SectionButton
                 title="Available on GitHub"
                 icon={<GitHubIcon className={clsx('my-2 h-16 w-16')} />}
                 description="Access powerful and flexible package on GitHub with MIT license."
                 active={currentState === 'github'}
                 onClick={() => setCurrentState('github')}
-              />
-              <SectionButton
+              />}
+              {isAvailable("npm") && <SectionButton
                 title="npm package"
                 icon={<NpmIcon className={clsx('my-2 h-16 w-16')} />}
                 description="Install and use the package with ease thanks to its typed options."
                 active={currentState === 'npm'}
                 onClick={() => setCurrentState('npm')}
-              />
-              <SectionButton
+              />}
+              {isAvailable("web") && <SectionButton
                 title="Website"
                 icon={<WebIcon className={clsx('my-2 h-16 w-16')} />}
                 description="Visit the website to learn more about the package and its features."
                 active={currentState === 'web'}
                 onClick={() => setCurrentState('web')}
-              />
+              />}
             </div>
           </div>
           <div className={clsx('w-full', 'lg:w-auto')}>
@@ -56,43 +94,27 @@ function ProjectsContents() {
               <div className={clsx('w-full', 'lg:h-[400px] lg:w-[600px]')}>
                 <AppWindow
                   type="browser"
-                  browserTabs={[
-                    {
-                      icon: <GitHubIcon className="h-4 w-4" />,
-                      title: 'enjidev/tailwindcss-accent - GitHub',
-                      isActive: currentState === 'github',
-                    },
-                    {
-                      icon: <NpmIcon className="h-4 w-4" />,
-                      title: 'tailwindcss-accent - npm',
-                      isActive: currentState === 'npm',
-                    },
-                    {
-                      icon: <WebIcon className="h-4 w-4" />,
-                      title: 'tailwindcss-accent.com',
-                      isActive: currentState === 'web',
-                    },
-                  ]}
+                  browserTabs={[...getBrowserTabs()]}
                 >
-                  {currentState === 'github' && (
+                  {appWindows.github && currentState === 'github' && (
                     <GitHubWireframe
-                      author="enjidev"
-                      license="MIT"
-                      repository="tailwindcss-accent"
-                      description="Adds accent colors for more dynamic and flexible color utilization."
+                      author={appWindows.github.author}
+                      license={appWindows.github.license}
+                      repository={appWindows.github.repository}
+                      description={appWindows.github.description}
                     />
                   )}
-                  {currentState === 'npm' && (
+                  {appWindows.npm && currentState === 'npm' && (
                     <NpmWireframe
-                      packageName="tailwindcss-accent"
-                      description="Adds accent colors for more dynamic and flexible color utilization."
-                      isWithTypeScript
+                      packageName={appWindows.npm.packageName}
+                      description={appWindows.npm.description}
+                      isWithTypeScript={appWindows.npm.isWithTypeScript === "true"}
                     />
                   )}
-                  {currentState === 'web' && (
+                  {appWindows.web && currentState === 'web' && (
                     <Image 
-                      src="/assets/images/placeholder.svg"
-                      alt="tailwindcss-accent"
+                      src={appWindows.web.image}
+                      alt={appWindows.web.alt}
                       width={598}
                       height={318}
                     />
