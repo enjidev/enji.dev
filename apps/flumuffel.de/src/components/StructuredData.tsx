24a62ff3fb5261f4db0ff9257ee642900468e31e
Flumuffel <40270803+Flumuffel@users.noreply.github.com>
2024-02-02 09:49:29 +0100

    Create StructuredData.tsx

---
+++ apps/flumuffel.de/src/components/StructuredData.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function StructuredData({ data }) {
+  return (
+    <Head>
+      <script
+        key="structured-data"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+      />
+    </Head>
+  );
+}
