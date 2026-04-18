baeddc6c9dff70369097c258e0de43159d27a410
Thái Duy <33753040+thdangduy@users.noreply.github.com>
2026-04-18 17:11:06 +0700

    Test db

--- apps/enji.dev/src/utils/prisma.ts
+++ apps/enji.dev/src/utils/prisma.ts
@@ -2,17 +2,13 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// Ép Prisma phải đọc trực tiếp chuỗi kết nối này nếu biến môi trường bị hụt
-const dbUrl = "mongodb://admin:33015676e4aac2d25b57478da17613822ada39e1@10.0.0.74:27017/portfolio?authSource=admin";
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: dbUrl,
-      },
-    },
-  });
+// Hardcode thẳng vào đây, không qua biến môi trường, không qua schema luôn!
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: "mongodb://admin:33015676e4aac2d25b57478da17613822ada39e1@10.0.0.74:27017/portfolio?authSource=admin"
+    }
+  }
+});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
