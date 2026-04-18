ab8abac7628aefdda9e82c6f403172b179ff3295
Thái Duy <33753040+thdangduy@users.noreply.github.com>
2026-04-18 19:11:22 +0700

    Hello

--- apps/enji.dev/src/utils/prisma.ts
+++ apps/enji.dev/src/utils/prisma.ts
@@ -2,6 +2,17 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+// ÉP BUỘC dùng link Atlas, không thèm đọc .env luôn cho khỏi lỗi!
+const cloudConnectionString = "mongodb+srv://thdangduy_db_user:r0FieCxI7zljQmRf@mongodb.mqoiwug.mongodb.net/portfolio?retryWrites=true&w=majority&appName=mongodb";
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: cloudConnectionString,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
